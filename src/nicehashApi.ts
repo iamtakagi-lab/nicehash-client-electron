fda8249d592664f9910b64f2ac2c727b561cf6f3
takagi <iam.takagi.dev@gmail.com>
2022-08-17 18:09:25 +0900

    feat: 1.0.3

--- src/nicehashApi.ts
+++ src/nicehashApi.ts
@@ -4,7 +4,7 @@ import { Config } from "./config";
 import { stringify } from "querystring";
 
 const APP_NAME = "Nicehash" as const;
-const APP_VERSION = "1.0.2" as const;
+const APP_VERSION = "1.0.3" as const;
 const NICEHASH_API_HOST = "https://api2.nicehash.com" as const;
 const USER_AGENT =
   `${APP_NAME}/${APP_VERSION} (+https://github.com/iamtakagi/Nicehash)` as const;
